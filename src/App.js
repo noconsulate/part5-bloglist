db3533f160cdac8c2afdb20c5df1fdf3b251fed8
Janssen Kuhn <noconsulate@gmail.com>
2019-09-11 11:20:39 -0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import blogService from './services/blogs'
 import loginService from './services/login'
+import LoginForm from './components/loginForm'
+import Blog from './components/Blog'
 
 const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [user, setUser] = useState('')
+  const [user, setUser] = useState(null)
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    blogService.getAll().then(
+      blogs => setBlogs(blogs)
+    )
+  }, [])
+
+  const rows = () => blogs.map(blog => 
+    <Blog
+      key={blog.id}
+      blog={blog}
+    />
+  )
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -15,7 +31,6 @@ const App = () => {
       })
       blogService.setToken(user.token)
       setUser(user)
-      console.log(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
@@ -23,35 +38,22 @@ const App = () => {
     }
   }
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={( {target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
-  )
-
 
   return (
     <div>
-      <h1>Phonebook</h1>
-      {loginForm()}
+      <h1>Blogs</h1>
+      {user === null ? 
+      <LoginForm 
+        handleLogin={handleLogin} 
+        username={username}
+        password={password}
+        setUser={({target}) => setUsername(target.value)}
+        setPass={({target}) => setPassword(target.value)}
+      /> :
+      <ul>
+        {rows()}
+      </ul>
+      }
 
     </div>
   )
